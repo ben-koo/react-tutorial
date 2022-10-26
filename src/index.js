db59a0c4ddfb2da9446b72a8d3d2a2e474f3e546
ben-koo <63335599+ben-koo@users.noreply.github.com>
2022-10-26 09:11:25 +0800

    Square class to function

--- src/index.js
+++ src/index.js
@@ -2,26 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-class Square extends React.Component {
-    // constructor(props) {
-    //   super(props); //this is compulsory.
-    //   this.state = {
-    //     value: null,
-    //   };
-    // }
+function Square(props) {
+  return (
+    <button className='square' onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
 
-    render() {
-      return (
-        <button 
-        className="square" 
-        // onClick={() => console.log('click')}
-        onClick={() => this.props.onClick()}
-        >
-          {this.props.value}
-        </button>
-      );
-    }
-  }
+// class Square extends React.Component {
+//     // constructor(props) {
+//     //   super(props); //this is compulsory.
+//     //   this.state = {
+//     //     value: null,
+//     //   };
+//     // }
+
+//     render() {
+//       return (
+//         <button 
+//         className="square" 
+//         // onClick={() => console.log('click')}
+//         onClick={() => this.props.onClick()}
+//         >
+//           {this.props.value}
+//         </button>
+//       );
+//     }
+//   }
   
   class Board extends React.Component {
     constructor(props) {
