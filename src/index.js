be29302458a1877a63f34735f349439bdff785a7
ben-koo <63335599+ben-koo@users.noreply.github.com>
2022-10-26 13:59:27 +0800

    time travel function

--- src/index.js
+++ src/index.js
@@ -106,12 +106,13 @@ function Square(props) {
         history: [{
             squares: Array(9).fill(null),
           }],
+        stepNumber: 0,
         xIsNext: true,
       };
     }
 
     handleClick(i) {
-      const history = this.state.history;
+      const history = this.state.history.slice(0, this.state.stepNumber + 1);
       const current = history[history.length - 1];
       const squares = current.squares.slice();
       if(calculateWinner(squares) || squares[i]) {
@@ -122,13 +123,22 @@ function Square(props) {
         history: history.concat([{
           squares: squares,
         }]),
+        stepNumber: history.length,
         xIsNext: !this.state.xIsNext,
+      });
+    }
+
+    jumpTo(step) {
+      this.setState({
+        stepNumber: step,
+        xIsNext: (step % 2) === 0,
       });
     }
 
     render() {
       const history = this.state.history;
-      const current = history[history.length - 1];
+      const current = history[this.state.stepNumber];
+      // const current = history[history.length - 1];
       const winner = calculateWinner(current.squares);
 
       const moves = history.map((step, move) => {
@@ -136,7 +146,7 @@ function Square(props) {
           'Go to move #' + move :
           'Go to game start';
         return (
-          <li>
+          <li key={move}>
             <button onClick={() => this.jumpTo(move)}>{desc}</button>
           </li>
         );
